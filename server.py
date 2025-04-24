06d99d3b7711903a28a8d0ae6c382c2b33f0a451
MCP User <user@example.com>
2025-04-24 20:20:18 +0530

    Init 2

--- server.py
+++ server.py
@@ -1,20 +1,48 @@
+from google import genai
+from google.genai import types
+from PIL import Image
+from io import BytesIO
+import base64
+import os
+import time
+
+api_key = os.getenv('GEMINI_API_KEY')
+output_path = os.getenv('OUTPUT_IMAGE_PATH')
+client = genai.Client(api_key=api_key)
+
 # server.py
 from mcp.server.fastmcp import FastMCP
 
 # Create an MCP server
 mcp = FastMCP("Demo")
 
-
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
-
 @mcp.tool()
 def generate_image(prompt: str) -> str:
     """Generate an image from a prompt"""
+    contents = [prompt]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-exp-image-generation",
+        contents=contents,
+        config=types.GenerateContentConfig(
+            response_modalities=['TEXT', 'IMAGE']
+        )
+    )
+
+    for part in response.candidates[0].content.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = Image.open(BytesIO((part.inline_data.data)))
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+            image_path = os.path.join(output_path, f"gemini_image_{int(time.time())}.png")
+            image.save(image_path)
+            print(f"Image saved to: {image_path}")
+            return image_path
+    
+    return "No image generated"
+
+
     
 
 
